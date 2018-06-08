091196dfd489a20a9ad0cb5d0d48bbf8210def7c
Kit Macleod <kitmacleod@gmail.com>
2018-06-08 11:09:53 +0100

    Added config etc

---
+++ webpack.config.js
@@ -0,0 +1,25 @@
+const path = require('path');
+
+module.exports = {
+  entry:'./src/main.js',
+  devServer:{
+    contentBase: path.join(__dirname, 'public'),
+     historyApiFallback: true
+  },
+  output: {
+    path: path.join(__dirname, 'public'),
+    filename: 'bundle.js'
+  },
+  module: {
+    rules: [
+      {
+        test: /\.s?css$/,
+        use: [
+          'style-loader',
+          'css-loader',
+          'sass-loader'
+        ]
+      }]
+  },
+  devtool: 'cheap-module-eval-source-map',
+};
